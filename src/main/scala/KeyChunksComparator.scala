20cd901075935d1f8dc4235e6e5e4c7004850c7f
VMois <vmois@protonmail.com>
2023-09-07 19:41:27 +0200

    Add combinational key chunks comparator

---
+++ src/main/scala/KeyChunksComparator.scala
@@ -0,0 +1,67 @@
+package compaction_unit
+
+import chisel3._
+import chisel3.util._
+
+
+/** A class for combinational Key Chunks Comparator
+ * This module is used to compare key chunks and decide whatever more chunks should be loaded or there is a winner.
+ * "mastIn" is mask that indicates which inputs should be included in the comparison.
+ * "maskOut" is an updated mask that needs to be passed for the next comparison.
+ * 
+ * Note: "maskOut" can be used to determine which chunks to load from memory in the next round.
+ *       Because only those chunks will be needed for comparison.
+ *
+ * More details about how this module works can be found in the tests.
+ *
+ *  @param busWidth, the number of bits that each number has.
+ *  @param n, how many numbers will be compared.
+ */
+class KeyChunksComparator(busWidth: Int = 4, n: Int = 4) extends Module {
+    val io = IO(new Bundle {
+        val in = Input(Vec(n, UInt(busWidth.W)))
+        val maskIn = Input(UInt(n.W))
+        val lastChunksMask = Input(UInt(n.W))
+
+        val maskOut = Output(UInt(n.W))
+        val haveWinner = Output(Bool())
+        val shouldLoadNewChunks = Output(Bool())
+    })
+    // 1. Apply mask to inputs to include/exclude certain inputs from the comparison based on maskIn value.
+    val modifiedInputs = Wire(Vec(n, UInt((busWidth + 1).W)))
+
+    io.in.zipWithIndex.map { case (input, index) =>
+        modifiedInputs(index) := Mux(io.maskIn(index) === 1.U, "b0".U ## input, "b1".U ## input)
+    }
+
+    // 2. Get the index of a smallest key chunk
+    val smallestIndex = Wire(UInt(log2Ceil(n).W))
+    val indexesArray = VecInit(Seq.tabulate(n)(i => i.U(log2Ceil(n).W)))
+
+    smallestIndex := indexesArray.reduce { (indexA, indexB) =>
+        Mux(modifiedInputs(indexA) <= modifiedInputs(indexB), indexA, indexB)
+    }
+
+    // 3. Compare all inputs with a smallest key chunk
+    val equalityMask = Wire(Vec(n, Bool()))
+
+    for (i <- 0 until n) {
+        equalityMask(i) := Mux(modifiedInputs(smallestIndex) === modifiedInputs(i), true.B, false.B)
+    }
+
+    // 4. If there is only one input that is equal to a smallest key chunk, then we have a winner.
+    // TODO: how efficient is PopCount?
+    val countOnes = PopCount(equalityMask.asUInt)
+    val hasOnlyOneOne = countOnes === 1.U
+
+    // 5. If there more than one input that is equal to a smallest key chunk
+    //    then we need to check if there are more chunks available for those inputs.
+    // 6. If some chunks (equal to smallest key chunk) do not have more chunks available,
+    //    then those chunks will be considered as a winner because they are the shortest.
+    val andResult = equalityMask.asUInt & io.lastChunksMask
+    val andResultEqualsZero = andResult === 0.U
+
+    io.maskOut := Mux(hasOnlyOneOne || andResultEqualsZero, equalityMask.asUInt, andResult)
+    io.haveWinner := hasOnlyOneOne || ~andResultEqualsZero
+    io.shouldLoadNewChunks := andResultEqualsZero & ~hasOnlyOneOne
+}
---
+++ src/test/scala/KeyChunksComparatorSpec.scala
@@ -0,0 +1,54 @@
+package compaction_unit
+
+import chisel3._
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
+import chisel3.experimental.BundleLiterals._
+
+
+class KeyChunksComparatorSpec extends AnyFreeSpec with ChiselScalatestTester {
+    "Different input combinations produce correct results" in {
+        test(new KeyChunksComparator(busWidth = 4, n = 4)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+            val testCases = Seq(
+                // in3, in2, in1, in0, maskIn, lastChunks, maskOut, expectedWinner, shouldLoadChunks
+                // single winner, more chunks are available
+                (0xD, 0xC, 0xB, 0xA, "b1111", "b0000", "b0001", true, false),
+                (0xD, 0xC, 0xA, 0xB, "b1111", "b0000", "b0010", true, false),
+                (0xD, 0xA, 0xC, 0xB, "b1111", "b0000", "b0100", true, false),
+                (0xA, 0xB, 0xD, 0xB, "b1111", "b0000", "b1000", true, false),
+
+                (0xD, 0xB, 0xC, 0xA, "b1100", "b0000", "b0100", true, false),
+                (0xD, 0xB, 0xA, 0xC, "b0111", "b0000", "b0010", true, false),
+
+                // single winner, some chunks are available
+                (0xA, 0xA, 0xA, 0xA, "b1111", "b0100", "b0100", true, false),
+                (0xA, 0xA, 0xA, 0xA, "b1011", "b0001", "b0001", true, false),
+
+                // multiple winners, happens only when there are no more chunks available
+                (0xB, 0xA, 0xA, 0xA, "b1111", "b1111", "b0111", true, false),
+                (0xB, 0xA, 0xA, 0xA, "b1011", "b0011", "b0011", true, false),
+                (0xA, 0xA, 0xA, 0xA, "b1111", "b1111", "b1111", true, false),
+
+                // should load new chunks
+                (0xD, 0xC, 0xA, 0xA, "b1111", "b0000", "b0011", false, true),
+                (0xD, 0xA, 0xA, 0xA, "b1101", "b0000", "b0101", false, true),
+                (0xA, 0xA, 0xA, 0xA, "b1111", "b0000", "b1111", false, true),
+                (0xA, 0xA, 0xA, 0xA, "b1011", "b0100", "b1011", false, true),
+            )
+
+            for ((in3, in2, in1, in0, maskIn, lastChunks, maskOut, expectedWinner, shouldLoadChunks) <- testCases) {
+                dut.io.in(0).poke(in0.U)
+                dut.io.in(1).poke(in1.U)
+                dut.io.in(2).poke(in2.U)
+                dut.io.in(3).poke(in3.U)
+
+                dut.io.lastChunksMask.poke(lastChunks.U)
+                dut.io.maskIn.poke(maskIn.U)
+
+                dut.io.maskOut.expect(maskOut.U)
+                dut.io.haveWinner.expect(expectedWinner.B)
+                dut.io.shouldLoadNewChunks.expect(shouldLoadChunks.B)
+            }
+        }
+    }
+}
