d8c620f87c89f2761f7c3a69b16977e617f4a35b
VMois <vmois@protonmail.com>
2023-09-19 10:09:22 +0200

    Add Key Buffer that loads key chunks row-by-row
    closes #14

---
+++ src/main/scala/KeyBuffer.scala
@@ -0,0 +1,127 @@
+package compaction_unit
+
+import chisel3._
+import chisel3.util._
+
+
+class KeyBufferIO(busWidth: Int, numberOfBuffers: Int) extends Bundle {
+    val enq = Flipped(Decoupled(UInt(busWidth.W)))
+    val deq = Decoupled(UInt(busWidth.W))
+
+    val bufferInputSelect = Input(UInt(log2Ceil(numberOfBuffers).W))
+    val incrWritePtr = Input(Bool())
+
+    val bufferOutputSelect = Output(UInt(log2Ceil(numberOfBuffers).W))
+}
+
+
+class KeyBuffer(busWidth: Int, numberOfBuffers: Int, maximumKeySize: Int) extends Module {
+    assert (busWidth > 0, "Bus width must be greater than 0")
+    assert (numberOfBuffers > 0, "numberOfBuffers must be greater than 0")
+    assert (maximumKeySize > 0, "maximumKeySize width must be greater than 0")
+    assert (maximumKeySize % busWidth == 0, "Key size must be a multiple of bus width")
+
+    val io = IO(new KeyBufferIO(busWidth, numberOfBuffers))
+
+    val depth = maximumKeySize / busWidth
+    val memSize = depth * numberOfBuffers
+    val mem = SyncReadMem(memSize, UInt(busWidth.W))
+
+    def counter(depth: Int, incr: Bool): (UInt, UInt) = {
+        val cntReg = RegInit(0.U(log2Ceil(depth).W))
+        val nextVal = Mux(cntReg === (depth-1).U, 0.U, cntReg + 1.U)
+        when (incr) {
+            cntReg := nextVal
+        }
+        (cntReg, nextVal)
+    }
+
+    val incrRead = WireInit(false.B)
+    val incrWrite = WireInit(false.B)
+    val (readPtr, nextRead) = counter(depth, incrRead)
+    val (writePtr, nextWrite) = counter(depth, incrWrite)
+
+    val emptyReg = RegInit(true.B)
+    val fullReg = RegInit(false.B)
+
+    val idle :: valid :: waitForTransfer :: Nil = Enum(3)
+    val stateReg = RegInit(idle)
+    val shadowReg = RegInit(0.U(busWidth.W))
+
+    val bufferOutputSelect = RegInit(0.U(log2Ceil(numberOfBuffers).W))
+
+    val shouldIncreaseReadPtr = bufferOutputSelect === (numberOfBuffers-1).U
+
+    val readFullPtr = readPtr * numberOfBuffers.U + bufferOutputSelect
+    val writeFullPtr = writePtr * numberOfBuffers.U + io.bufferInputSelect
+
+    when (io.enq.valid && !fullReg) {
+        mem.write(writeFullPtr, io.enq.bits)
+        fullReg := nextWrite === readPtr && io.incrWritePtr
+
+        // row was loaded, next write will be applied to the next row
+        when (io.incrWritePtr) {
+            incrWrite := true.B
+            emptyReg := false.B
+        }
+    }
+
+    val data = mem.read(readFullPtr)
+
+    switch(stateReg) {
+        is(idle) {
+            when(!emptyReg) {
+                stateReg := valid
+                fullReg := false.B
+                emptyReg := nextRead === writePtr && shouldIncreaseReadPtr
+                // prepare for the next read, as it requires one cycle delay
+                bufferOutputSelect := bufferOutputSelect + 1.U
+            }
+        }
+        is(valid) {
+            when(io.deq.ready) {
+                when(!emptyReg) {
+                    stateReg := valid
+                    fullReg := false.B
+                    emptyReg := nextRead === writePtr && shouldIncreaseReadPtr
+                    incrRead := shouldIncreaseReadPtr
+                    bufferOutputSelect := bufferOutputSelect + 1.U
+                } otherwise {
+                    bufferOutputSelect := 0.U
+                    stateReg := idle
+                }
+            } otherwise {
+                shadowReg := data
+                stateReg := waitForTransfer
+            }
+        }
+        is(waitForTransfer) {
+            when(io.deq.ready) {
+                when(!emptyReg) {
+                    stateReg := valid
+                    fullReg := false.B
+                    emptyReg := nextRead === writePtr && (bufferOutputSelect === (numberOfBuffers-1).U)
+                    incrRead := shouldIncreaseReadPtr
+                    bufferOutputSelect := bufferOutputSelect + 1.U
+                } otherwise {
+                    bufferOutputSelect := 0.U
+                    stateReg := idle
+                }
+            }
+        }
+    }
+
+    io.deq.bits := Mux(stateReg === valid, data, shadowReg)
+    io.enq.ready := !fullReg
+    io.deq.valid := stateReg === valid || stateReg === waitForTransfer
+
+    // TODO: this is a hack to output the correct buffer select value
+    //       the reason is that the "bufferOutputSelect" register 
+    //       will contain the value from the current cycle but the valid data will be from previous.
+    io.bufferOutputSelect := bufferOutputSelect - 1.U
+}
+
+object KeyBufferMain extends App {
+  println("Generating the Key Buffer Verilog...")
+  (new chisel3.stage.ChiselStage).emitVerilog(new KeyBuffer(4, 4, 8), Array("--target-dir", "generated"))
+}
---
+++ src/test/scala/KeyBufferSpec.scala
@@ -0,0 +1,125 @@
+package compaction_unit
+
+import chisel3._
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
+import chisel3.experimental.BundleLiterals._
+
+
+class KeyBufferSpec extends AnyFreeSpec with ChiselScalatestTester {
+    "Should load key chunks rows and read them back" in {
+        test(new KeyBuffer(busWidth = 4, numberOfBuffers = 4, maximumKeySize = 8)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+            // setup default values
+            dut.io.deq.ready.poke(false.B)
+            dut.io.enq.valid.poke(true.B)
+            dut.io.incrWritePtr.poke(false.B)
+
+            // Write two rows of key chunks
+            for (i <- 0 until 4) {
+                dut.io.enq.ready.expect(true.B)
+                dut.io.enq.bits.poke((0xA + i).U)
+                dut.io.bufferInputSelect.poke(i.U)
+                if (i == 3) {
+                    dut.io.incrWritePtr.poke(true.B)
+                }
+                dut.clock.step()
+                dut.io.incrWritePtr.poke(false.B)
+            }
+
+            for (i <- 0 until 4) {
+                dut.io.enq.ready.expect(true.B)
+                dut.io.enq.bits.poke((0x3 + i).U) 
+                dut.io.bufferInputSelect.poke(i.U)
+                if (i == 3) {
+                    dut.io.incrWritePtr.poke(true.B)
+                }
+                dut.clock.step()
+                dut.io.incrWritePtr.poke(false.B)
+            }
+            dut.io.enq.valid.poke(false.B)
+            dut.io.deq.ready.poke(true.B)
+
+            // Read two rows of key chunks
+            for (i <- 0 until 4) {
+                dut.io.deq.valid.expect(true.B)
+                dut.io.deq.bits.expect((0xA + i).U)
+                dut.io.bufferOutputSelect.expect(i.U)
+                dut.clock.step()
+            }
+
+            for (i <- 0 until 4) {
+                dut.io.deq.valid.expect(true.B)
+                dut.io.deq.bits.expect((0x3 + i).U)
+                dut.io.bufferOutputSelect.expect(i.U)
+                dut.clock.step()
+            }
+
+            dut.io.deq.valid.expect(false.B)
+            dut.io.enq.ready.expect(true.B)
+        }
+    }
+
+    "Should load key chunks rows and read them back with delay" in {
+        test(new KeyBuffer(busWidth = 4, numberOfBuffers = 4, maximumKeySize = 12)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+            // setup default values
+            dut.io.deq.ready.poke(false.B)
+            dut.io.enq.valid.poke(true.B)
+
+            // Write first row of key chunks
+            for (i <- 0 until 4) {
+                dut.io.enq.ready.expect(true.B)
+                dut.io.enq.bits.poke((0xA + i).U)
+                dut.io.bufferInputSelect.poke(i.U)
+                if (i == 3) {
+                    dut.io.incrWritePtr.poke(true.B)
+                }
+                dut.clock.step()
+                dut.io.incrWritePtr.poke(false.B)
+            }
+
+            dut.io.enq.valid.poke(false.B)
+            
+            // wait
+            dut.clock.step(3)
+            dut.io.deq.ready.poke(true.B)
+            dut.io.enq.valid.poke(true.B)
+
+            // Write second row and read first row of key chunks
+            for (i <- 0 until 4) {
+                dut.io.deq.valid.expect(true.B)
+                dut.io.deq.bits.expect((0xA + i).U)
+                dut.io.bufferOutputSelect.expect(i.U)
+
+                dut.io.enq.ready.expect(true.B)
+                dut.io.enq.bits.poke((0x3 + i).U) 
+                dut.io.bufferInputSelect.poke(i.U)
+                if (i == 3) {
+                    dut.io.incrWritePtr.poke(true.B)
+                }
+                dut.clock.step()
+                dut.io.incrWritePtr.poke(false.B)
+            }
+
+            dut.io.enq.valid.poke(false.B)
+
+            // TODO: one clock delay is needed here
+            dut.clock.step()
+            
+            // Read second row of key chunks
+            for (i <- 0 until 4) {
+                dut.io.deq.valid.expect(true.B)
+                dut.io.deq.bits.expect((0x3 + i).U)
+                dut.io.bufferOutputSelect.expect(i.U)
+                dut.clock.step()
+            }
+
+            dut.io.deq.ready.poke(false.B)
+            dut.io.deq.valid.expect(false.B)
+            dut.io.enq.ready.expect(true.B)
+            dut.clock.step()
+
+            dut.io.deq.valid.expect(false.B)
+            dut.io.enq.ready.expect(true.B)
+        }
+    }
+}
