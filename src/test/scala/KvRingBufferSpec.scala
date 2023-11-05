VMois/LSM-Compactron3000

commit 66e330b62e27c66242b447a15cb25bf14b7a69dc

Do not use multiple ports for memory in KvRingBuffer
- Vivado synthesis cannot properly generate bitstream because it complains about amount of ports used by memory;
- looks like using when/else and even switch statements create unnecessary ports (looking at generated Verilog);
- this commit only writes to memory from a single location, address is chosen with multiple Muxes;
- this commit fixes the synthesis.

src/main/scala/KvRingBuffer.scala
@@ -133,30 +133,34 @@ class KVRingBuffer(depth: Int, busWidth: Int = 4, keySize: Int = 8, valueSize: I
     val inputStateReg = RegInit(writeData)
     val outputStateReg = RegInit(requestKeyLen)
 
+    val writeReg = RegInit(0.U(busWidth.W))
+    val offset = (metadataAddressOffset + keyAddressOffset + valueAddressOffset).U
+    val writeDataPtr = Mux(inputStateReg === writeData, Mux(io.isInputKey, metadataAddressOffset.U, (metadataAddressOffset + keyAddressOffset).U) + Mux(io.isInputKey, writeKeyChunkPtr, writeValueChunkPtr), 0.U)
+    val metadataOffsetPtr = Mux(inputStateReg === inputSaveValueLen, 1.U, 0.U)
+
+    mem.write(writePtr * offset + writeDataPtr + metadataOffsetPtr, Mux(inputStateReg === writeData, io.enq.bits, writeReg))
+
     switch(inputStateReg) {
         is(writeData) {
             when(io.enq.valid && !fullReg) {
                 when (io.isInputKey) {
-                    mem.write(writePtr * (metadataAddressOffset + keyAddressOffset + valueAddressOffset).U + metadataAddressOffset.U + writeKeyChunkPtr, io.enq.bits)
                     writeKeyChunkPtr := writeKeyChunkPtr + 1.U
                 } otherwise {
-                    mem.write(writePtr * (metadataAddressOffset + keyAddressOffset + valueAddressOffset).U + (metadataAddressOffset + keyAddressOffset).U + writeValueChunkPtr, io.enq.bits)
                     writeValueChunkPtr := writeValueChunkPtr + 1.U
-
                     when(io.lastInput) {
                         inputStateReg := inputSaveKeyLen
+                        writeReg := writeKeyChunkPtr
                     }
                 }
             }
         }
 
         is(inputSaveKeyLen) {
-            mem.write(writePtr * (metadataAddressOffset + keyAddressOffset + valueAddressOffset).U, writeKeyChunkPtr)
             inputStateReg := inputSaveValueLen
+            writeReg := writeValueChunkPtr
         }
 
         is(inputSaveValueLen) {
-            mem.write(writePtr * (metadataAddressOffset + keyAddressOffset + valueAddressOffset).U + 1.U, writeValueChunkPtr)
             emptyReg := false.B
             incrWrite := true.B
             fullReg := nextWrite === readPtr
@@ -176,7 +180,7 @@ class KVRingBuffer(depth: Int, busWidth: Int = 4, keySize: Int = 8, valueSize: I
         }
     }
 
-    val readFullPtr = readPtr * (metadataAddressOffset + keyAddressOffset + valueAddressOffset).U + readValueChunkPtr + readKeyChunkPtr
+    val readFullPtr = readPtr * offset + readValueChunkPtr + readKeyChunkPtr
     val data = mem.read(readFullPtr)
     val shadowReg = RegInit(0.U(busWidth.W))
 

commit 324e6f2ab8ea442f842da0bd0bda66c7e9cb054a

Fix overwriting memory when buffer is full and valid or metadata write does not occur
- this bug was introduced in commit 66e330b;
- existing tests did not catch it cause it only happens when buffer is full.

src/main/scala/KvRingBuffer.scala
@@ -145,7 +145,10 @@ class KVRingBuffer(depth: Int, busWidth: Int = 4, keySize: Int = 8, valueSize: I
     val writeDataPtr = Mux(inputStateReg === writeData, Mux(io.isInputKey, metadataAddressOffset.U, (metadataAddressOffset + keyAddressOffset).U) + Mux(io.isInputKey, writeKeyChunkPtr, writeValueChunkPtr), 0.U)
     val metadataOffsetPtr = Mux(inputStateReg === inputSaveValueLen, 1.U, 0.U)
     val writeFullPtr = writePtr * offset + writeDataPtr + metadataOffsetPtr
-    mem.write(writeFullPtr, Mux(inputStateReg === writeData, io.enq.bits, writeReg))
+
+    when (io.enq.valid || inputStateReg === inputSaveKeyLen || inputStateReg === inputSaveValueLen) {
+        mem.write(writeFullPtr, Mux(inputStateReg === writeData, io.enq.bits, writeReg))
+    }
 
     switch(inputStateReg) {
         is(writeData) {

src/test/scala/KvRingBufferSpec.scala
@@ -28,8 +28,8 @@ class KvRingBufferSpec extends AnyFreeSpec with ChiselScalatestTester {
         }
     }
 
-    "Should be not ready when full" in {
-        test(new KVRingBuffer(2, busWidth = 4, keySize = 8, valueSize = 8, metadataSize = 8)) { dut =>
+    "Should not be ready when full" in {
+        test(new KVRingBuffer(2, busWidth = 4, keySize = 8, valueSize = 8, metadataSize = 8)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
             setDefaultValues(dut)
             dut.clock.step()
 
@@ -45,10 +45,14 @@ class KvRingBufferSpec extends AnyFreeSpec with ChiselScalatestTester {
             dut.io.enq.bits.poke(0xC.U)
             dut.clock.step()
             dut.io.enq.valid.poke(false.B)
+            dut.io.lastInput.poke(false.B)
 
             while (dut.io.enq.ready.peek().litToBoolean == false) {
                 dut.clock.step()
             }
+
+            dut.io.status.empty.expect(false.B)
+            dut.io.status.full.expect(false.B)
 
             // Write KV pair 2
             dut.io.enq.bits.poke(0xB.U)
@@ -59,8 +63,79 @@ class KvRingBufferSpec extends AnyFreeSpec with ChiselScalatestTester {
             dut.io.isInputKey.poke(false.B)
             dut.io.lastInput.poke(true.B)
             dut.io.enq.bits.poke(0xD.U)
-            dut.clock.step(3) // wait for metadata to write
 
+            
+            dut.clock.step() // wait for metadata to write
+            dut.io.lastInput.poke(false.B)
+            dut.io.enq.valid.poke(false.B)
+            dut.clock.step(2)
+
+            dut.io.status.full.expect(true.B)
+            dut.io.status.empty.expect(false.B)
+            dut.io.enq.ready.expect(false.B)
+        }
+    }
+
+    "Should not overwrite memory if valid is not set and buffer is full" in {
+        test(new KVRingBuffer(2, busWidth = 4, keySize = 8, valueSize = 8, metadataSize = 8)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+            setDefaultValues(dut)
+            dut.clock.step()
+
+            // Write KV pair 1
+            dut.io.enq.ready.expect(true.B)
+            dut.io.enq.bits.poke(0xA.U)
+            dut.io.enq.valid.poke(true.B)
+            dut.io.isInputKey.poke(true.B)
+            dut.clock.step()
+
+            dut.io.isInputKey.poke(false.B)
+            dut.io.lastInput.poke(true.B)
+            dut.io.enq.bits.poke(0xC.U)
+            dut.clock.step()
+            dut.io.enq.valid.poke(false.B)
+            dut.io.lastInput.poke(false.B)
+
+            while (dut.io.enq.ready.peek().litToBoolean == false) {
+                dut.clock.step()
+            }
+
+            dut.io.status.empty.expect(false.B)
+            dut.io.status.full.expect(false.B)
+
+            // Write KV pair 2
+            dut.io.enq.bits.poke(0xB.U)
+            dut.io.enq.valid.poke(true.B)
+            dut.io.isInputKey.poke(true.B)
+            dut.clock.step()
+
+            dut.io.isInputKey.poke(false.B)
+            dut.io.lastInput.poke(true.B)
+            dut.io.enq.bits.poke(0xD.U)
+
+            
+            dut.clock.step() // wait for metadata to write
+            dut.io.lastInput.poke(false.B)
+            dut.io.enq.valid.poke(false.B)
+            dut.clock.step(2)
+
+            dut.io.status.full.expect(true.B)
+            dut.io.status.empty.expect(false.B)
+            dut.io.enq.ready.expect(false.B)
+
+            // Read KV pair 1
+            dut.io.deq.ready.poke(true.B)
+            dut.io.deq.bits.expect(0xA.U)
+            dut.io.deq.valid.expect(true.B)
+            dut.io.outputKeyOnly.poke(false.B)
+            dut.clock.step()
+            
+            dut.io.deq.bits.expect(0xC.U)
+            dut.io.deq.valid.expect(true.B)
+            dut.clock.step()
+
+            dut.io.deq.ready.poke(false.B)
+            
+            // We did not move a pointer
             dut.io.status.full.expect(true.B)
             dut.io.status.empty.expect(false.B)
             dut.io.enq.ready.expect(false.B)