5ea73bb22b50420b03561367cd84b48c600d27c8
VMois <vmois@protonmail.com>
2023-11-08 17:07:34 +0100

    Add ControlAdapter to connect AXI interface to Controller
    - we will need to connect the same clock to two different inputs one for "Chisel" clock and another for S_AXI_ACLK required by ControlAdapter verilog code; not sure how to fix this, not very big issue so let's leave it for now;
    - on the other hand, having two different resets, "Chisel" one and from ControlAdapter verilog is good as Chisel modules works with the inverse reset.

---
+++ src/main/scala/ControlAdapter.scala
@@ -0,0 +1,42 @@
+package compaction_unit
+
+import chisel3._
+import chisel3.util.HasBlackBoxResource
+
+class AxiIO extends Bundle {
+    val S_AXI_ACLK = Input(Bool())
+    val S_AXI_ARESETN = Input(Bool())
+    val S_AXI_AWADDR = Input(UInt(4.W))
+    val S_AXI_AWPROT = Input(UInt(3.W))
+    val S_AXI_AWVALID = Input(Bool())
+    val S_AXI_AWREADY = Output(Bool())
+    val S_AXI_WDATA = Input(UInt(32.W))
+    val S_AXI_WSTRB = Input(UInt(4.W))
+    val S_AXI_WVALID = Input(Bool())
+    val S_AXI_WREADY = Output(Bool())
+    val S_AXI_BRESP = Output(UInt(2.W))
+    val S_AXI_BVALID = Output(Bool())
+    val S_AXI_BREADY = Input(Bool())
+    val S_AXI_ARADDR = Input(UInt(4.W))
+    val S_AXI_ARPROT = Input(UInt(3.W))
+    val S_AXI_ARVALID = Input(Bool())
+    val S_AXI_ARREADY = Output(Bool())
+    val S_AXI_RDATA = Output(UInt(32.W))
+    val S_AXI_RRESP = Output(UInt(2.W))
+    val S_AXI_RVALID = Output(Bool())
+    val S_AXI_RREADY = Input(Bool())
+}
+
+trait ExternalControllerSignals extends Bundle {
+    // custom signals for Controller
+    val status = Input(UInt(32.W))
+    val control = Output(UInt(32.W))
+}
+
+
+class ControlAdapterVerilog extends BlackBox with HasBlackBoxResource {
+    val io = IO(new Bundle with ExternalControllerSignals {
+        val axi = new AxiIO
+    })
+    addResource("/control_adapter_axi_lite_s.v")
+}
